b7b740f3aa015a984775674032461069b073b9db
Albert Chyb <albert7773000@gmail.com>
2024-08-13 23:13:40 +0200

    Removed unnecessary typehint

--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
 
       <main
         className={
-          'mt-[number:calc(var(--header-height)+var(--main-content-top-spacing))] container'
+          'mt-[calc(var(--header-height)+var(--main-content-top-spacing))] container'
         }
       >
         {children}
