8726308f8e942b02b9ee49dcecc4289604595bf9
Albert Chyb <albert7773000@gmail.com>
2024-08-14 20:52:31 +0200

    Tested theme provider

--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -4,12 +4,14 @@ import { z } from 'zod';
 
 /**
  * The default theme for the application.
+ *
+ * This theme is used when no other theme is stored in local storage.
  */
-const DEFAULT_THEME: Theme = 'device-theme-sync';
+export const DEFAULT_THEME: Theme = 'device-theme-sync';
 
 /**
  * React context for managing the application's theme and syncing it with the device's theme.
- * Provides the current theme, sync state, and functions to change the theme or enable/disable sync.
+ * Provides the current theme and function to change the theme.
  */
 export const ThemeContext = createContext<ThemeContextValue>({
   currentTheme: DEFAULT_THEME,
@@ -23,10 +25,13 @@ export const ThemeContext = createContext<ThemeContextValue>({
  * @param props The component's props, including children to be rendered within the provider.
  * @returns The ThemeContext provider component with the application theme state.
  */
-export function ThemeProvider({ children }: PropsWithChildren) {
+export function ThemeProvider({
+  children,
+  defaultTheme = DEFAULT_THEME,
+}: ThemeProviderProps) {
   const [appTheme, setAppTheme] = useLocalStorage({
     key: 'theme',
-    initialValue: DEFAULT_THEME,
+    initialValue: defaultTheme,
     zodSchema: ThemesEnumSchema,
   });
 
@@ -51,14 +56,32 @@ export function ThemeProvider({ children }: PropsWithChildren) {
 }
 
 /**
+ * An array of valid CSS class names that correspond to the different themes available and are applied to the `<html>` tag
+ * to change the application's theme.
+ */
+export const THEMES_CSS_CLASSES = [
+  'dark',
+  'light',
+  'device-theme-sync',
+] as const;
+
+/**
  * A Zod schema that defines the allowed theme values.
  * The values are also CSS classes that are applied to the `<html>` tag.
  */
-export const ThemesEnumSchema = z.enum(['dark', 'light', 'device-theme-sync']);
+export const ThemesEnumSchema = z.enum(THEMES_CSS_CLASSES);
+
 /**
  * Type representing the application's theme.
  */
 export type Theme = z.infer<typeof ThemesEnumSchema>;
+
+/**
+ * The props for the ThemeProvider component.
+ */
+export type ThemeProviderProps = PropsWithChildren<{
+  defaultTheme?: Theme;
+}>;
 
 /**
  * The context value type for the ThemeContext.
---
+++ tests/contexts/theme.test.tsx
@@ -0,0 +1,100 @@
+import {
+  Theme,
+  ThemeContext,
+  ThemeProvider,
+  THEMES_CSS_CLASSES,
+} from '@/contexts/theme';
+import { render, screen, waitFor } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+
+describe('ThemeProvider', () => {
+  /** Renders all elements required for testing  */
+  let renderThemeProvider: (theme?: Theme) => void;
+
+  /** Reads current theme from a consumer */
+  let readCurrentTheme: () => Theme;
+
+  /** Changes the theme from a consumer */
+  let changeTheme: (theme: Theme) => void;
+
+  beforeEach(() => {
+    renderThemeProvider = (theme?: Theme) => {
+      render(
+        <ThemeProvider defaultTheme={theme}>
+          <ThemeContext.Consumer>
+            {({ currentTheme, setTo }) => {
+              changeTheme = setTo;
+
+              return (
+                <div
+                  data-testid='theme-context-consumer'
+                  data-current-theme={currentTheme}
+                />
+              );
+            }}
+          </ThemeContext.Consumer>
+        </ThemeProvider>
+      );
+    };
+
+    afterEach(() => {
+      for (const themeClass of THEMES_CSS_CLASSES) {
+        document.documentElement.classList.remove(themeClass);
+      }
+    });
+
+    readCurrentTheme = () => {
+      return screen
+        .getByTestId('theme-context-consumer')
+        .getAttribute('data-current-theme') as Theme;
+    };
+  });
+
+  it('should sync to device theme by default', () => {
+    renderThemeProvider();
+
+    expect(readCurrentTheme()).toBe('device-theme-sync');
+  });
+
+  it('should allow a consumer to read current theme', () => {
+    renderThemeProvider('dark');
+
+    expect(readCurrentTheme()).toBe('dark');
+  });
+
+  it('should allow a consumer to change the current theme', async () => {
+    renderThemeProvider('dark');
+    changeTheme('device-theme-sync');
+
+    await waitFor(() => expect(readCurrentTheme()).toBe('device-theme-sync'));
+  });
+
+  it('should add a CSS class to the <html> element when the light theme is chosen', async () => {
+    renderThemeProvider('light');
+
+    await waitFor(() => expect(document.documentElement).toHaveClass('light'));
+  });
+
+  it('should add a CSS class to the <html> element when the dark theme is chosen', async () => {
+    renderThemeProvider('dark');
+
+    await waitFor(() => expect(document.documentElement).toHaveClass('dark'));
+  });
+
+  it('should add a CSS class to the <html> element when synchronizing with device theme is enabled', async () => {
+    renderThemeProvider('device-theme-sync');
+
+    await waitFor(() =>
+      expect(document.documentElement).toHaveClass('device-theme-sync')
+    );
+  });
+
+  it('should previous theme`s CSS class when changing to a new theme', async () => {
+    renderThemeProvider('dark');
+    changeTheme('light');
+
+    await waitFor(() =>
+      expect(document.documentElement).not.toHaveClass('dark')
+    );
+  });
+});
