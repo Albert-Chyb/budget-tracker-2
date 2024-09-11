0321fd43149e8c6d17dc78f4fd1235ebf1716123
Albert Chyb <albert7773000@gmail.com>
2024-09-11 17:30:29 +0200

    Fixed import path

--- src/components/transactions/category.tsx
+++ src/components/transactions/category.tsx
@@ -1,8 +1,8 @@
-import { TCategory } from '@/lib/types/category';
+import { TCategory } from '@/lib/db-schemas/category';
 
 export default function Category({ category }: CategoryProps) {
   return (
-    <h3 className='p-5 text-3xl font-bold uppercase text-center'>
+    <h3 className='font-bold uppercase text-center'>
       {category.name}
     </h3>
   );
