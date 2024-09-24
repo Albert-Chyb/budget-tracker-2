ac7d548926bec106225f10c0ceeca731e04b5aab
Albert Chyb <albert7773000@gmail.com>
2024-09-24 11:24:50 +0200

    Logged route error to the console

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,3 +1,12 @@
+import { useEffect } from 'react';
+import { useRouteError } from 'react-router-dom';
+
 export default function ErrorPage() {
+  const error = useRouteError();
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
   return <h1>Napotkano niespodziewany błąd !</h1>;
 }
