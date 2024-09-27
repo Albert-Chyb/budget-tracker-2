e72dcfe19ed55da25d7cd21483997bf1c26f1762
Albert-Chyb <albert7773000@gmail.com>
2024-09-27 16:05:53 +0200

    Moved logic of determining when a fetcher finished processing processing an action

--- src/components/cms/cms-context.tsx
+++ src/components/cms/cms-context.tsx
@@ -1,12 +1,6 @@
+import { useFetcherWithLifecycle } from '@/hooks/fetcher-with-lifecycle';
 import { useMediaQuery } from '@uidotdev/usehooks';
-import {
-  createContext,
-  PropsWithChildren,
-  useCallback,
-  useEffect,
-  useState,
-} from 'react';
-import { useFetcher } from 'react-router-dom';
+import { createContext, PropsWithChildren, useCallback } from 'react';
 import { SubmitTarget } from 'react-router-dom/dist/dom';
 import { useCMSEditorController } from './cms-editor-controller';
 
@@ -28,20 +22,13 @@ export const CMSContext = createContext<CMSContextValue>(CMS_CONTEXT_VALUE);
 
 export function CMSContextProvider(props: CMSContextProps) {
   const editor = useCMSEditorController();
-  const fetcher = useFetcher();
+  const fetcher = useFetcherWithLifecycle({
+    afterSubmit() {
+      editor.close();
+    },
+  });
   const isMobile = useMediaQuery('(max-width: 768px)');
   const { children } = props;
-  const [fetcherPrevState, setFetcherPrevState] = useState<string | null>(null);
-
-  useEffect(() => () => setFetcherPrevState(fetcher.state), [fetcher.state]);
-
-  useEffect(() => {
-    // Automatically close the editor after fetcher finished request
-
-    if (fetcherPrevState === 'submitting') {
-      editor.close();
-    }
-  }, [editor, fetcherPrevState]);
 
   const handleEditorSubmit = useCallback(
     (target) => {
