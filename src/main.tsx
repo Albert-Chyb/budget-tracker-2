fee5b8825e505600e1cd1f63e61c78a5fd3bab54
Albert-Chyb <albert7773000@gmail.com>
2024-10-05 16:37:30 +0200

    Moved react query client config to a separate file

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,23 @@
 import { UserProvider } from '@/contexts/user-context.tsx';
 import { initSupabase } from '@/lib/supabase/init.ts';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { ReactQueryClientProvider } from './contexts/react-query-context.tsx';
 import { ThemeProvider } from './contexts/theme.tsx';
 import './index.css';
 import { router } from './router-config.tsx';
 
 initSupabase();
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <ReactQueryClientProvider>
       <ThemeProvider>
         <UserProvider>
           <RouterProvider router={router} />
         </UserProvider>
       </ThemeProvider>
-    </QueryClientProvider>
+    </ReactQueryClientProvider>
   </React.StrictMode>
 );
