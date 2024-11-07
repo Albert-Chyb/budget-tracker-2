084eecf7d02104c8c395d3697edc05c43785e6dc
Albert Chyb <albert7773000@gmail.com>
2024-11-07 19:10:39 +0100

    Applied the filter on input change instead of on submit

--- src/components/cms/filters-forms/text-field-filter-form.tsx
+++ src/components/cms/filters-forms/text-field-filter-form.tsx
@@ -1,64 +1,37 @@
-import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useContext } from 'react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { Label } from '@radix-ui/react-label';
+import { useDebounce } from '@uidotdev/usehooks';
+import { useContext, useEffect, useId, useState } from 'react';
 import {
   CMSTableFilterContext,
   CMSTableFilterContextValue,
 } from '../cms-table-filters';
 
-const formSchema = z.object({
-  phrase: z.string(),
-});
-type FormValue = z.infer<typeof formSchema>;
-
 export function TextFieldFilterForm() {
-  const { setFilterValue, filterValue, close } = useContext(
+  const { setFilterValue, filterValue } = useContext(
     CMSTableFilterContext
   ) as CMSTableFilterContextValue<string>;
+  const [searchedTerm, setSearchedTerm] = useState(filterValue ?? '');
+  const debouncedTerm = useDebounce(searchedTerm, 300);
+  const inputId = useId();
 
-  const form = useForm<FormValue>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      phrase: typeof filterValue === 'string' ? filterValue : '',
-    },
-  });
-
-  function handleSubmit(formValue: FormValue) {
-    setFilterValue(formValue.phrase);
-    close();
-  }
+  useEffect(() => {
+    setFilterValue(debouncedTerm);
+  }, [debouncedTerm, setFilterValue]);
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-2'>
-        <FormField
-          control={form.control}
-          name='phrase'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Szukana fraza</FormLabel>
+    <form onSubmit={($event) => $event.preventDefault()}>
+      <Label htmlFor={inputId} className='inline-block mb-2'>
+        Wpisz szukaną frazę
+      </Label>
 
-              <FormControl>
-                <Input type='text' {...field} placeholder='Szukana fraza' />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-
-        <Button type='submit' className='w-full' variant='outline'>
-          Zastosuj
-        </Button>
-      </form>
-    </Form>
+      <Input
+        id={inputId}
+        type='text'
+        placeholder='Szukana fraza'
+        onChange={($event) => setSearchedTerm($event.target.value)}
+        value={searchedTerm}
+      />
+    </form>
   );
 }
